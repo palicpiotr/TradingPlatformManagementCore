31c5c76af0e1875428e4b8b33be39ba217e363c1
Piotr Palic <petr.palic@altenar.com>
2018-06-06 23:09:55 +0300

    refactor

--- TradingPlatformManagement/TradingPlatformManagement/Extensions/EmailSenderExtensions.cs
+++ TradingPlatformManagement/TradingPlatformManagement/Extensions/EmailSenderExtensions.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
-using TradingPlatformManagement.Services;
 
 namespace TradingPlatformManagement.Services
 {
