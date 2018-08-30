2342a6e1b79d5157874ee8607876caeccac6ab5e
Piotr Palic <piotr.palic@ledgerleopard.com>
2018-08-30 19:09:59 +0300

    Fake1

--- TradingPlatformManagement/TradingPlatformManagement/Controllers/HomeController.cs
+++ TradingPlatformManagement/TradingPlatformManagement/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace TradingPlatformManagement.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
     }
 }
